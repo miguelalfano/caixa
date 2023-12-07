2a786d7d42b59a585e4dcba8ab9b735778a9eef3
miguelalfano <137218522+miguelalfano@users.noreply.github.com>
2023-12-07 18:35:54 -0300

    Update login.controller.js

--- login/login.controller.js
+++ login/login.controller.js
@@ -1,15 +1,19 @@
-var lastUserId = 0;
-
 async function adicionarUsuario() {
   // Capturar os valores do formulário
   var nome = document.getElementById("modal-nome").value;
   var senha = document.getElementById("modal-senha").value;
 
-  // Incrementar o userId
-  lastUserId += 1;
+  if (!nome || !senha) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  // Gerar um ID aleatório de 5 dígitos
+  var id = Math.floor(10000 + Math.random() * 90000).toString().slice(-5);
+
   // Dados a serem adicionados
   var dados = {
-    userId: lastUserId,
+    id: id,
     nome: nome,
     senha: senha,
   };
@@ -19,10 +23,19 @@ async function adicionarUsuario() {
   try {
     const usuarioCadastrado = await new LoginService().novoUsuario(dados);
     console.log(usuarioCadastrado);
+    alert("Seu usuário foi criado com sucesso");
+
+    // Limpar o campo de senha
+    document.getElementById("modal-nome").value = "";
+    document.getElementById("modal-senha").value = "";
+
   } catch (e) {
     alert("Ocorreu um erro ao cadastrar um novo usuário");
   }
 }
+
+
+
 
 async function login() {
   localStorage.removeItem('user')
